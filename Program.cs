26f67a5048a494499d0f47a73a84d131daa9672c
kia-h <46kia-h@users.noreply.github.com>
2020-04-22 19:41:36 +1000

    squash them

--- Program.cs
+++ Program.cs
@@ -6,26 +6,39 @@ using System.Threading.Tasks;
 
 namespace top10
 {
+
+    public static class TestExt
+    {
+        public static string M1(this string str)
+        {
+            return str.ToUpper();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string filePath = @"C:\Users\kia.heydari\source\repos\top10\Pop.csv";
-            CsvReader reader = new CsvReader(filePath);
-            Country[] countries = reader.ReadFirstNCountries(10);
-            List<Country> countriesAll = reader.ReadFirstAllCountries();
+            string str = "heloo world";
+            Console.WriteLine("uisng static method.staticMethod "+TestExt.M1(str)  );
 
-            //to read top 10 from array
-            foreach (Country country in countries)
-            {
-                Console.WriteLine($"{ country.Population} : { country.Name}");
-            }
 
-            //to read from list
-            foreach (Country country in countriesAll)
-            {
-                Console.WriteLine($"{ country.Population} : { country.Name}");
-            }
+            Console.WriteLine("using extension method version " + str.M1()); ;
+            //string filePath = @"C:\Users\kia.heydari\source\repos\top10\Pop.csv";
+            //CsvReader reader = new CsvReader(filePath);
+            //Country[] countries = reader.ReadFirstNCountries(10);
+            //List<Country> countriesAll = reader.ReadFirstAllCountries();
+
+            ////to read top 10 from array
+            //foreach (Country country in countries)
+            //{
+            //    Console.WriteLine($"{ country.Population} : { country.Name}");
+            //}
+
+            ////to read from list
+            //foreach (Country country in countriesAll)
+            //{
+            //    Console.WriteLine($"{ country.Population} : { country.Name}");
+            //}
 
             Console.ReadKey();
         }
